b962068416166357a0ee4e5d18e6d3987cc535cf
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-18 21:35:15 -0700

    adding demo code and cats model

---
+++ src/models/cats.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import storage from '../lib/storage/data-store.js';
+import uuid from 'uuid/v1';
+
+class Cat{
+
+  constructor(config) {
+    this.id = uuid();
+    this.name = config && config.name || '';
+    this.age = config && config.age || '';
+  }
+
+  save() {
+    return storage.save(this);
+  }
+
+  static fetchAll() {
+    return storage.getAll();
+  }
+
+  static findOne(id) {
+    return storage.get(id);
+  }
+
+  static updateOne(criteria) {
+    return storage.update(this);
+  }
+
+  static deleteOne(id) {
+    return storage.delete(id);
+  }
+
+}
+
+export default Cat;
